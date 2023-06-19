70469e9d976e16d6400f715a151e285b51cc3497
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-19 22:30:29 +0530

    updated server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+//modules
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
@@ -5,6 +6,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
 
+//midddlewares
 app.use(cors());
 app.use(express.json());
 
