f696f5e8ced32c3d00f876891a6958d70e46a9a6
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-22 09:37:40 +0530

    header style modified

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 import "./header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header className="wrapper">
-            <div className="logo">
-                <img
-                    src="https://images.unsplash.com/photo-1567446537708-ac4aa75c9c28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
-                    alt="logo"
-                />
-            </div>
+            <Link
+                to="/"
+                className="logo"
+            >
+                HB
+            </Link>
             <div className="right">
                 <div className="profile">
                     <img
