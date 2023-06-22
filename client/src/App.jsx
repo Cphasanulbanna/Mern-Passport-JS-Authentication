3adaaff267e4eb4af2c01074dd2ba7c27a0b53ad
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-22 08:00:13 +0530

    added sass , header styling completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Post from "./components/post/Post";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+                <Route
+                    path="/post/:id"
+                    element={<Post />}
+                />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import "./header.scss";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className="wrapper">
+            <div className="logo">
+                <img
+                    src="https://images.unsplash.com/photo-1567446537708-ac4aa75c9c28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+                    alt="logo"
+                />
+            </div>
+            <div className="right">
+                <div className="profile">
+                    <img
+                        src="https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+                        alt="profile"
+                    />
+                </div>
+                <h1>banna</h1>
+                <div className="logout-button">Logout</div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
