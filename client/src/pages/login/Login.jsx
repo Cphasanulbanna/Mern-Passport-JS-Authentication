8a122d928a8dab0c9e7a3156a6be101f72fa8939
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-22 09:34:31 +0530

    login page styling completed

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 
+import "./login.scss";
+
+import google from "../../assets/images/google.png";
+import facebook from "../../assets/images/facebook.png";
+import github from "../../assets/images/github.png";
+
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <div className="login-container">
+            <div className="wrapper">
+                <h1>Choose a login method</h1>
+                <div className="left">
+                    <div className="login-button">
+                        <div className="logo">
+                            <img
+                                src={google}
+                                alt="google"
+                            />
+                        </div>
+                        <h2>google</h2>
+                    </div>
+                    <div className="login-button">
+                        <div className="logo">
+                            <img
+                                src={facebook}
+                                alt="fb"
+                            />
+                        </div>
+                        <h2>facebook</h2>{" "}
+                    </div>
+                    <div className="login-button">
+                        <div className="logo">
+                            <img
+                                src={github}
+                                alt="github"
+                            />
+                        </div>
+                        <h2>github</h2>{" "}
+                    </div>
+                </div>
+                <div className="middle">
+                    <div className="line" />
+                    <div className="or">OR</div>
+                </div>
+                <div className="right">
+                    <form action="">
+                        <input
+                            type="text"
+                            placeholder="username"
+                            value={""}
+                        />
+                        <input
+                            type="text"
+                            placeholder="password"
+                            value={""}
+                        />
+                        <button type="sumbit">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
